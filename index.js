1de3e5e449e1a218d2d2b31a9f485c61b7b1f23a
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-10-21 18:35:06 +0200

    Added displayQuiz function

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var correctAnswer;
 var incorrectAnswers;
 var quiz = new Object();
 var quizArray = new Array();
-
+var quizAPIData;
 
 window.onload = function () {
 };
@@ -22,49 +22,46 @@ window.onload = function () {
 function startQuiz() {
     difficulty = document.querySelector("input[name=difficultyLevel]:checked").value;
     totalQuestion = document.querySelector("#totalQuestions").value;
+    quizURL = prepareQuizURL(totalQuestion, difficulty);
+    fetch(quizURL)
+        .then((data) => data.json())
+        .then(function (data) {
+            quizAPIData = data.results;
+            displayQuiz();
+        });
+};
 
+function displayQuiz() {
     document.querySelector("#examOptions").style.display = "none";
     document.querySelector("#exam").style.display = "block";
     document.querySelector("#progress").style.display = "block";
     document.querySelector("#nextQuestionButton").style.display = "none";
     document.querySelector("#answersArea").innerHTML = "";
-    quizURL = prepareQuizURL(1, difficulty);
-    //Bug: Question can repeat as the server may send same question twice.
-
-    fetch(quizURL)
-        .then((data) => data.json())
-        .then(function (data) {
-            question = data.results[0].question;
-
-            document.querySelector("#question").innerText = question;
-            correctAnswer = data.results[0].correct_answer;
-            console.log(correctAnswer);
-
-            incorrectAnswers = data.results[0].incorrect_answers;
-
-
-            var a = currentQuestion + 1;
-            document.querySelector("#questionNumber").innerText = "Question " + a + " of " + totalQuestion;
-
-            var options = randomizeQuizAnswers(correctAnswer, incorrectAnswers);
-
-            for (i = 0; i < options.length; i++) {
-                var newOption = document.createElement("input");
-                newOption.type = "radio";
-                newOption.name = "answer";
-                newOption.id = "answer" + i;
-                newOption.className = "form-check-input";
-                newOption.value = options[i];
-                var newLabel = document.createElement("label");
-                newLabel.innerText = options[i];
-                newLabel.htmlFor = "answer" + i;
-                newLabel.className = "form-check-label";
-                var breakLine = document.createElement("br");
-                document.querySelector("#answersArea").appendChild(newOption);
-                document.querySelector("#answersArea").appendChild(newLabel);
-                document.querySelector("#answersArea").appendChild(breakLine);
-            };
-        });
+    question = quizAPIData[currentQuestion].question;
+    document.querySelector("#question").innerText = question;
+    correctAnswer = quizAPIData[currentQuestion].correct_answer;
+    console.log("correct: " + correctAnswer);
+    incorrectAnswers = quizAPIData[currentQuestion].incorrect_answers;
+    console.log("incorrect: " + incorrectAnswers);
+    var a = currentQuestion + 1;
+    document.querySelector("#questionNumber").innerText = "Question " + a + " of " + totalQuestion;
+    var options = randomizeQuizAnswers(correctAnswer, incorrectAnswers);
+    for (i = 0; i < options.length; i++) {
+        var newOption = document.createElement("input");
+        newOption.type = "radio";
+        newOption.name = "answer";
+        newOption.id = "answer" + i;
+        newOption.className = "form-check-input";
+        newOption.value = options[i];
+        var newLabel = document.createElement("label");
+        newLabel.innerText = options[i];
+        newLabel.htmlFor = "answer" + i;
+        newLabel.className = "form-check-label";
+        var breakLine = document.createElement("br");
+        document.querySelector("#answersArea").appendChild(newOption);
+        document.querySelector("#answersArea").appendChild(newLabel);
+        document.querySelector("#answersArea").appendChild(breakLine);
+    };
     document.querySelector("#answerStatus").innerText = "Please select an answer and then press Check.";
     document.querySelector("#answerStatus").className = "row alert alert-dark";
     document.querySelector(".progress-bar").style.width = ((parseInt(currentQuestion) + 1) / parseInt(totalQuestion)) * 100 + "%";
@@ -157,7 +154,7 @@ function nextQuestion() {
         document.querySelector("#exam").innerHTML = "";
         return
     } else {
-        startQuiz(currentQuestion);
+        displayQuiz();
         document.querySelector("#checkButton").style.display = "inline-block";
         document.querySelector(".progress-bar").style.width = ((parseInt(currentQuestion) + 1) / parseInt(totalQuestion)) * 100 + "%";
     };
